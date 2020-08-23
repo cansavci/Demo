c5c1dd1e75997b1eb6434bdd20e3a4d84eb4c914
cansavci <mcansavci@gmail.com>
2020-08-23 19:43:02 +0300

    Backup v3

--- DemoApplication/ClientApp/src/components/Demo.tsx
+++ DemoApplication/ClientApp/src/components/Demo.tsx
@@ -12,6 +12,7 @@ import { ApplicationState } from "../store/index2";
 import { connect } from "react-redux";
 import MaterialTable, { Column } from "material-table";
 import Upload from "./Upload";
+import Axios from "axios";
 
 type DemoProps = DemoStore.AssetsVariantState & // ... state we've requested from the Redux store
   typeof DemoStore.actionCreators & // ... plus action creators we've requested
@@ -20,11 +21,19 @@ type DemoProps = DemoStore.AssetsVariantState & // ... state we've requested fro
 const DemoPage: React.FunctionComponent<DemoProps> = (props) => {
   const [assetData, setAssetData] = React.useState<DemoStore.Asset[]>([]);
   const [folderData, setFolderData] = React.useState<DemoStore.Folder[]>([]);
-  const [variantsData, setVariantsData] = React.useState<DemoStore.Variant[]>([]);
+  const [variantsData, setVariantsData] = React.useState<DemoStore.Variant[]>(
+    []
+  );
+  //for error handling
+  const [iserror, setIserror] = React.useState(false);
+  const [errorMessages, setErrorMessages] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     props.requestAssets();
-    setAssetData({ ...props.assets });
+    //setAssetData({ ...props.assets });
+    setAssetData([
+      { id: 1, name: "aa", folderId: 1, variants: [] } as DemoStore.Asset,
+    ]);
   }, []);
 
   const columns: any = [
@@ -33,10 +42,79 @@ const DemoPage: React.FunctionComponent<DemoProps> = (props) => {
     { title: "folderId", field: "folderId" },
   ];
 
+  const handleRowAdd = (newData, resolve) => {
+    Axios.post("/assets", newData)
+      .then((res) => {
+        let dataToAdd = [...data];
+        dataToAdd.push(newData);
+        setData(dataToAdd);
+        resolve();
+        setErrorMessages([]);
+        setIserror(false);
+      })
+      .catch((error) => {
+        setErrorMessages(["Cannot add data. Server error!"]);
+        setIserror(true);
+        resolve();
+      });
+  };
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    Axios.patch("/assets/" + newData.id, newData)
+      .then((res) => {
+        const dataUpdate = [...data];
+        const index = oldData.tableData.id;
+        dataUpdate[index] = newData;
+        setData([...dataUpdate]);
+        resolve();
+        setIserror(false);
+        setErrorMessages([]);
+      })
+      .catch((error) => {
+        setErrorMessages(["Update failed! Server error"]);
+        setIserror(true);
+        resolve();
+      });
+  };
+
+  const handleRowDelete = (oldData, resolve) => {
+    Axios.delete("/assets/" + oldData.id)
+      .then((res) => {
+        const dataDelete = [...data];
+        const index = oldData.tableData.id;
+        dataDelete.splice(index, 1);
+        setData([...dataDelete]);
+        resolve();
+      })
+      .catch((error) => {
+        setErrorMessages(["Delete failed! Server error"]);
+        setIserror(true);
+        resolve();
+      });
+  };
+
   return (
     <React.Fragment>
       <Icon>Can</Icon>
-      {/* <MaterialTable title="Sample table" columns={columns} data={assetData} /> */}
+      <MaterialTable
+        title="Sample table"
+        columns={columns}
+        data={assetData}
+        editable={{
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve) => {
+              handleRowUpdate(newData, oldData, resolve);
+            }),
+          onRowAdd: (newData) =>
+            new Promise((resolve) => {
+              handleRowAdd(newData, resolve);
+            }),
+          onRowDelete: (oldData) =>
+            new Promise((resolve) => {
+              handleRowDelete(oldData, resolve);
+            }),
+        }}
+      />
       <Upload></Upload>
     </React.Fragment>
   );
